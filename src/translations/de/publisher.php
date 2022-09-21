0d9d1c7dff2c9c801acd518fce189940a2eb2797
lukasNo1 <lukas.wegmueller@goldinteractive.ch>
2022-09-21 12:45:30 +0200

    Remove "save" warning as entries are now autosaved

--- src/translations/de/publisher.php
+++ src/translations/de/publisher.php
@@ -4,7 +4,6 @@ return [
     'No pending publish dates'                         => 'Keine Einträge sind zur Veröffentlichung vorgesehen',
     'Pending publish dates'                            => 'Zu veröffentlichende Einträge',
     'Schedule a publish date'                          => 'Veröffentlichung erstellen',
-    'Entry must be saved otherwise you will lose data' => 'Eintrag muss gespeichert sein, ansonsten kann Datenverlust entstehen',
     'Add publish date'                                 => 'Veröffentlichung hinzufügen',
     'Publish date'                                     => 'Veröffentlichungsdatum',
     'Draft'                                            => 'Entwurf',
